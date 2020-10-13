e2f973d63a82ade10e45a1012bcc85bdf853e833
Serenity Button <serenitybutton@ituness-MacBook-Pro.local>
2020-10-13 13:48:41 -0500

    Created variableExercise.js file

---
+++ variablesExercise.js
@@ -0,0 +1,19 @@
+let myAge = 18;
+
+myAge = 13;
+
+
+myAge += 6;
+
+myAge --;
+
+
+myAge = "None of your business";
+
+console.log(myAge);
+
+let blank;
+blank = null;
+
+let notANumber = 0 * 4;
+console.log(notANumber);
